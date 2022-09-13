3c7cc421f0c1077c0adaa7943c8e95fc5c265be2
Franco Zivcovich <franco.zivcovich@gmail.com>
2022-09-13 18:32:22 +0200

    Added clear_integrators.m which clear the exponential integrators with their persistent variables.

---
+++ numerical_experiments/exponential_integrators/clear_integrators.m
@@ -0,0 +1,24 @@
+function clear_integrators()
+  % clear integrators to prevent them to recycle information when they should not.
+
+
+  clear cuschRS21_bamphi
+  clear cuschRS21_kiops
+
+  clear cuschss68_bamphi
+  clear cuschss68_kiops
+
+  clear epirk4s3a_bamphi
+  clear epirk4s3a_kiops
+
+  clear exprk4s6_bamphi
+  clear exprk4s6_kiops
+
+  clear exptg2s1_bamphi
+  clear exptg2s1_kiops
+
+  clear stochexprk1_bamphi
+  clear stochexprk1_kiops
+
+
+end
